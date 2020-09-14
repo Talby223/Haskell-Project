f7f123dfece18f713a8c600618c609b3e1fdd74d
Wesley <Generaldov@gmail.com>
2020-09-13 19:33:44 -0500

    This is awful

---
+++ src/DoubleUp.hs
@@ -0,0 +1,16 @@
+module DoubleUp where 
+    
+doubleMe :: Int -> Int
+doubleMe x = x + x
+
+doubleUs :: Num a => a -> a -> a
+doubleUs x y = x*2 + y*2   
+
+doubleSmallNumber :: (Ord a, Num a) => a -> a
+doubleSmallNumber x = if x > 100  
+                        then x  
+                        else x*2   
+
+doubleSmallNumber' :: (Num a, Ord a) => a -> a
+doubleSmallNumber' x = (if x > 100 then x else x*2) + 1  
+
