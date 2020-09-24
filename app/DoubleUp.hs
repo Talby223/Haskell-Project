9cebffbd0e1fa00155e03de81dd8ba8196fd8502
Wesley <Generaldov@gmail.com>
2020-09-24 16:47:36 -0500

    Update DoubleUp.hs

--- app/DoubleUp.hs
+++ app/DoubleUp.hs
@@ -103,12 +103,29 @@ butHow this = do
    
 --      Seconds -> convert to Days -> Calculate Age
 
-data Planet = Planet { planetName       :: String
-                       , orbitalPeriod  :: Int
-                       , orbitalSeconds :: Int
-                        } deriving (Show)
+data Planet = Mercury
+            | Venus
+            | Earth
+            | Mars
+            | Jupiter
+            | Saturn
+            | Uranus
+            | Neptune 
 
-data Planets = Mercury Float | Venus Float | Earth Float | Mars Float | Jupiter Float | Saturn Float | Uranus Float | Neptune Float deriving (Show)
+orbitalPeriod :: Planet -> Float
+orbitalPeriod planet =
+  case planet of
+    Mercury ->  0.2408467 
+    Venus ->  0.61519726 
+    Earth ->  1.0
+    Mars ->  1.8808158 
+    Jupiter ->  11.862615 
+    Saturn ->  29.447498 
+    Uranus ->  84.016846 
+    Neptune ->  164.79132 
+  
+
 
 ageOn :: Planet -> Float -> Float
-ageOn :: Earth 
+ageOn planet seconds =
+     seconds / 31557600.0 / orbitalPeriod planet
